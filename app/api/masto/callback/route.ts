fcf9735f1cb7d5fcbcaa148c626ebc08466a5308
Chinmay Sa <68277286+ScreenTechnicals@users.noreply.github.com>
2025-11-11 01:35:45 +0530

    fix: fix reviews

--- app/api/masto/callback/route.ts
+++ app/api/masto/callback/route.ts
@@ -34,7 +34,7 @@ export async function GET(req: Request) {
 
         const redirectUrl = new URL(
             `/profile?token=${tokenData.access_token}&username=${me.username}`,
-            process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+            process.env.NEXTAUTH_URL! || 'http://localhost:3000'
         );
 
         return NextResponse.redirect(redirectUrl);
